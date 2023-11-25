3a1ea5d5120a1901da8595b635ed037f48af6427
Saicharan maddimsetti <147791999+Saicharan7777@users.noreply.github.com>
2023-11-25 22:25:01 +0530

    #include<stdio.h> int main() {     int a,pi=3.14;     float c;     scanf("%d",&a);     c=(3.14*a*a*a)*4/3;     printf("%.2f",c); }

--- Volume_of_Sphere.c
+++ Volume_of_Sphere.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    float b;
+    int a,pi=3.14;
+    float c;
     scanf("%d",&a);
-    b=(3.14*a*a*a)*4/3;
-    printf("%.2f",b);
+    c=(3.14*a*a*a)*4/3;
+    printf("%.2f",c);
 }
