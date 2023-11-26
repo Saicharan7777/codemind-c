9bab07ee6c99b0a06a834da2d99f2f77638264c3
Saicharan maddimsetti <147791999+Saicharan7777@users.noreply.github.com>
2023-11-26 11:50:41 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     if(a>b){         printf("%d",a);     }else{         printf("%d",b);     } }

--- Height.c
+++ Height.c
@@ -3,12 +3,9 @@ int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    if (a>b)
-    {
+    if(a>b){
         printf("%d",a);
-    }
-    else 
-    {
+    }else{
         printf("%d",b);
     }
 }
