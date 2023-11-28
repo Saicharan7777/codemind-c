c78307725b1c5da154303c94eac042bb177fe6c8
Saicharan maddimsetti <147791999+Saicharan7777@users.noreply.github.com>
2023-11-28 20:57:45 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     c=5*a;     if(c>=b)     {       printf("YES");
        }else{
            printf("NO");
        }
    }

---
+++ Reach_Home.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=5*a;
+    if(c>=b)
+    {
+      printf("YES");
+      
+    }else{
+        printf("NO");
+    }
+}
