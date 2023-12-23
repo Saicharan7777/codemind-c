508519c98fff345f110e6694631f214875c45bec
Saicharan maddimsetti <147791999+Saicharan7777@users.noreply.github.com>
2023-12-23 11:58:25 +0530

    #include<stdio.h> int main() {     int N,i,j;     scanf("%d",&N);     for(i=1;i<=N;i++)     {         for(j=1;j<=i;j++)         {             printf("* ");         }         printf(" ");     } }

--- Basic_Pattern_2.1.c
+++ Basic_Pattern_2.1.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int N,i,j;
+    scanf("%d",&N);
+    for(i=1;i<=N;i++)
     {
         for(j=1;j<=i;j++)
         {
