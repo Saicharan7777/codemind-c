f117e2c037264e3822db559940d621746b71e742
Saicharan maddimsetti <147791999+Saicharan7777@users.noreply.github.com>
2023-12-30 23:00:50 +0530

    #include<stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         for(j=1;j<=i;j++)         {     printf("*");         }     printf(" ");
        }
    }

--- printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
+    int a,i,j;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
         for(j=1;j<=i;j++)
         {
-            printf("*");
-            
+    printf("*");
         }
-        printf("
+    printf("
 ");
-    }
     
+        
+    }
 }
